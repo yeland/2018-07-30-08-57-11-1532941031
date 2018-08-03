abef11c61adb1287afbfbeef90be8924998b4a06
yeland <wlx100715@163.com>
2018-08-03 17:34:15 +0800

    complete main

--- main/main.js
+++ main/main.js
@@ -1,4 +1,33 @@
 module.exports = function main() {
     console.log("Debug Info");
     return 'Hello World!';
+	
+  if(arguments.length===0){
+	return 0;
+  }
+  
+  var toplcd = ['._.','...','._.','._.','...','._.','._.','._.','._.','._.'];
+  var midlcd = ['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'];
+  var botlcd = ['|_|','..|','|_.',',_|','..|','._|','|_|','..|','|_|','..|'];
+  
+  var str = arguments[0];
+  var length = str.length;
+  var arr;
+  arr.length = length;
+  for(var i=0;i<length;i++){
+	  arr[i] = parseInt(str.charAt(i));
+  }
+  
+  var toparr;
+  var midarr;
+  var botarr;
+  
+  for(var i=0;i<length;i++){
+	  toparr[i] = toplcd[arr[i]];
+	  midarr[i] = midlcd[arr[i]];
+	  botarr[i] = botlcd[arr[i]];
+  }
+  
+  console.log(toparr.join(" ")) + "\n" + midarr.join(" ") + "\n" + botarr.join(" ");
+  
 };
